23d84d1de4f13a38dabe68e56dff2b610abdd2d0
Prentice Wongvibulsin <me@prenticew.com>
2010-04-19 18:52:55 -0700

    Added license text and header to index page

--- src/index.php
+++ src/index.php
@@ -1,4 +1,28 @@
 <?PHP
+/*
+ *  Copyright (c) 2010 Cal Poly Engineering Student Council
+ *
+ *  Developers:
+ *    Brian Oppenheim <brianopp@gmail.com> 
+ *    Prentice Wongvibulsin <me@prenticew.com>
+ *
+ *  This file is part of eScan.
+ *
+ *   eScan is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   eScan is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with eScan.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 header("Location: user");
 ?>
 
