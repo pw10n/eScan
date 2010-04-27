329f76e667dfb7f89920a30ace98bb833fdb0efa
Prentice Wongvibulsin <me@prenticew.com>
2010-04-26 18:33:23 -0700

    Add headers. Closes #21

--- src/admin/generate_pins_view.php
+++ src/admin/generate_pins_view.php
@@ -1,4 +1,33 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+
+/*
+ *  Copyright (c) 2010 Cal Poly Engineering Student Council
+ *
+ *  Developers:
+ *    Brian Oppenheim <brianopp@gmail.com> 
+ *    Prentice Wongvibulsin <me@prenticew.com>
+ *
+ *  Project URL:
+ *    http://www.prenticew.com/escan
+ *
+ *  This file is part of eScan.
+ *
+ *   eScan is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   eScan is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with eScan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
--- src/admin/model.php
+++ src/admin/model.php
@@ -1,4 +1,30 @@
 <?php
+/*
+ *  Copyright (c) 2010 Cal Poly Engineering Student Council
+ *
+ *  Developers:
+ *    Brian Oppenheim <brianopp@gmail.com> 
+ *    Prentice Wongvibulsin <me@prenticew.com>
+ *
+ *  Project URL:
+ *    http://www.prenticew.com/escan
+ *
+ *  This file is part of eScan.
+ *
+ *   eScan is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   eScan is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with eScan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 require_once "../dbconn.php";
 require_once "../common.php";
