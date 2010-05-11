9e79cd6f38579fbbf0ac609279998d3c92853b74
Prentice Wongvibulsin <me@prenticew.com>
2010-05-10 18:18:22 -0700

    add setup.php

---
+++ setup.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *  Copyright (c) 2010 Cal Poly Engineering Student Council
+ *
+ *  Developers:
+ *    Brian Oppenheim <brianopp@gmail.com> 
+ *    Prentice Wongvibulsin <me@prenticew.com>
+ *
+ *  Project URL:
+ *    http://www.prenticew.com/escan
+ *
+ *  This file is part of eScan.
+ *
+ *   eScan is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   eScan is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with eScan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// SETUP DATABASE
+//    arguments: database name, admin username, admin password
+//    precondition: database name is not used, admin account is valid
+//    postcondition: database is initialized
+function init_database($dbname, $user, $pass){
+
+}
+
+// GENERATE .htaccess/.htpasswd FILES
+//    arguments: admin, password 
+//    precondition: none
+//    postcondition: htpasswd file with admin user is created and 
+//                   htaccess restricts access to admin folder
+function init_htfiles($user, $pass){
+   
+}
+
+
+
+
+
+?>
